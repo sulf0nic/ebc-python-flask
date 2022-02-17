fd883eb772a294a9f310fbb0c2ccc75361ffb468
Ricardo A <99375284+sulf0nic@users.noreply.github.com>
2022-02-16 21:27:01 -0600

    v.1.1
    version estable

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from crypt import methods
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for,redirect
+from database import tasks
 
 #Inicializar
 app = Flask(__name__)
 
 @app.route("/")
 def Index():
-    return render_template("Index.html")
+    return render_template("Index.html", todoList = tasks)
     #return "<a href='#'>Hello Word Flask :))</a>"
     #return "<h1>hello world flask</h1>"
     #return "<a href='#'>hello world flask</a>"
@@ -19,9 +20,12 @@ def HolaEBC():
 def UnGetRandom():
   return "Hola ebc/Random"
 
-##@app.route("/", methods=['POST'])
-##def AddNewTask():
-##    if request.method == 'POST':
+@app.route("/newTask", methods=['POST'])
+def AddNewTask():
+    if request.method == 'POST':
+      newTask = {"id": len(tasks)+1, "name": request.form['task_name']}     
+      tasks.append(newTask)
+      return redirect(url_for('Index'))
 
 if __name__ == "__main__":
   app.run(port=5000, debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 tasks = [
-    {"id":1, "name":"lavar los platos."}, 
+    {"id":1, "name":'Lavar los platos.'}, 
     {"id":2, "name":"Sacar a pasear el perro."}, 
-    {"id":3, "name":"Comprar el foco de cocina."}, 
+    {"id":3, "name":"Comprar el foco de cocina."},
+    {"id":4, "name":"Barrer"},
+    {"id":5, "name":"Trapear"},
     ]
-    
